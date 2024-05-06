18b6c43737c0bb55472caa2c5d4a575058d6e3b9
Jennie Brockhoff <jbrockh1@gmail.com>
2024-05-06 09:43:41 -0500

    Added form to contacts from bootstrap- need to edit but basic form is there

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,7 +1,49 @@
-export default function Contact() {
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+function Contact() {
   return (
     <div>
-      <h1>Contact Page</h1>
+      <InputGroup className="mb-3">
+        <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+        <Form.Control
+          placeholder="Username"
+          aria-label="Username"
+          aria-describedby="basic-addon1"
+        />
+      </InputGroup>
+
+      <InputGroup className="mb-3">
+        <Form.Control
+          placeholder="Recipient's username"
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+        />
+        <InputGroup.Text id="basic-addon2">@example.com</InputGroup.Text>
+      </InputGroup>
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="basic-url">Your vanity URL</Form.Label>
+        <InputGroup>
+          <InputGroup.Text id="basic-addon3">
+            https://example.com/users/
+          </InputGroup.Text>
+          <Form.Control id="basic-url" aria-describedby="basic-addon3" />
+        </InputGroup>
+      </Form.Group>
+
+      <InputGroup className="mb-3">
+        <InputGroup.Text>$</InputGroup.Text>
+        <Form.Control aria-label="Amount (to the nearest dollar)" />
+        <InputGroup.Text>.00</InputGroup.Text>
+      </InputGroup>
+
+      <InputGroup>
+        <InputGroup.Text>With textarea</InputGroup.Text>
+        <Form.Control as="textarea" aria-label="With textarea" />
+      </InputGroup>
+
       <p>
         Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
         molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
@@ -18,3 +60,5 @@ export default function Contact() {
     </div>
   );
 }
+
+export default Contact;
